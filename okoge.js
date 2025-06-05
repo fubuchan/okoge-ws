06080cf7a1803bd9331007a2248b5ab1c9215331
fubuchan <122428744+fubuchan@users.noreply.github.com>
2025-06-06 00:42:47 +0900

    Update okoge.js

---
+++ okoge.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+  console.log('クライアント接続');
+  ws.on('message', (message) => {
+    console.log('受信:', message);
+    ws.send('okogeから応答！');
+  });
+});
+
+server.listen(process.env.PORT || 10000, () => {
+  console.log('サーバ起動');
+});
